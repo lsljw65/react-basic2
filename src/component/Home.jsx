797eb7fdebbe0e104cf5957bf37eda4ff47d7fb6
lee <lsljw65@gmail.com>
2024-05-30 19:58:43 +0900

    props

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,13 @@
+// Props: 상위컴포넌트에서 하위컴포넌트로 파라미터를 전달하는 것.
+
+import Props from "./pages/Props";
+
 const Home=()=>{
     return(
         <>
             <div className="home">
                 <h2>Home.jsx</h2>
+                <Props name="대한민국" age="12"/>
             </div>
         </>
     )
---
+++ src/component/pages/Props.jsx
@@ -0,0 +1,42 @@
+
+
+const Props=(props)=>{
+    return(
+        <>
+            <div className="props">
+                <h2>Props.jsx</h2>
+                <p>
+                    props값 : {props.name}, {props.age}
+                </p>
+            </div>
+        </>
+    )
+}
+
+/* function Props(props){
+    return(
+        <>
+            <div className="props">
+                <h2>Props.jsx</h2>
+                <p>
+                    props값 : {props.name}, {props.age}
+                </p>
+            </div>
+        </>
+    )
+} */
+
+export default Props;
+
+/* export default function Props(props){
+    return(
+        <>
+            <div className="props">
+                <h2>Props.jsx</h2>
+                <p>
+                    props값 : {props.name}, {props.age}
+                </p>
+            </div>
+        </>
+    )
+} */
